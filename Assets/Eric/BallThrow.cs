8a750be8921af6e9812c239fa90d5deedb9b5cd5
nineteen-eightyfour <ermorgan@protonmail.com>
2020-12-09 19:33:51 -0800

    It moves when you touch it. It seems I have much to learn of vectors.

--- Assets/Eric/BallThrow.cs
+++ Assets/Eric/BallThrow.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class BallThrow : MonoBehaviour
 {
+    private Rigidbody2D rb2;
+    public float tx;
+    public float ty;//throwx+y
+
     // Start is called before the first frame update
     void Start()
     {
+        rb2 = GetComponent<Rigidbody2D>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    private void OnMouseDown()
+    {
+        rb2.gravityScale = 1;
+        rb2.AddForce(new Vector2(tx, ty));
     }
 }
