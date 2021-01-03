cb6ac2cf6160e5d32f281ed20a8c3827876eef75
isaac <isaac@DESKTOP-M97TLDN.local>
2021-01-02 17:40:08 -0800

    quit buttton, taking break cause it pissed me off

---
+++ Assets/Isaac/EndGame.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndGame : MonoBehaviour
+{
+    public void quit()
+    {
+        Debug.Log("has quit game");
+        Application.Quit();
+
+    }
+}
