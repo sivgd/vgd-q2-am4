7ecc67beca52f9190b23eec576f2d813502a537d
Eric <ermorgan@protonmail.com>
2021-01-04 19:11:48 -0800

    Multishot functional! Press 2 to see.

--- Assets/Eric/BallThrow.cs
+++ Assets/Eric/BallThrow.cs
@@ -5,35 +5,70 @@ using UnityEngine;
 public class BallThrow : MonoBehaviour
 {
     private Rigidbody2D rb2;
+    private CircleCollider2D col;
     public float scale;
     public bool thrown = false;
     public Vector3 initialPos;
     public bool active = false;
+    public bool isBasic = true;
+    public bool isMult = false;
+    public Vector3 upspeed; //what's upspeed?
+    public Vector3 downspeed; //what's upspeed?
 
     // Start is called before the first frame update
     void Start()
     {
         rb2 = GetComponent<Rigidbody2D>();
+        col = GetComponent<CircleCollider2D>();
         initialPos = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Alpha1) == true)
+        if (Input.GetKeyDown(KeyCode.Alpha1) == true && isBasic == true)
         {
             active = true;
         }
-    }
-    private void OnMouseUp()
-    {
-        if (thrown == false && active == true)
+        else if (Input.GetKeyDown(KeyCode.Alpha1) == true && isBasic == false)
+        {
+            active = false;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2) == true && isMult == true)
+        {
+            active = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2) == true && isMult == false)
+        {
+            active = false;
+        }
+        if (active == false)
+        {
+            gameObject.layer = 8; //And now, for my disappearing trick!
+            col.enabled = false;
+        }
+        else
+        {
+            gameObject.layer = 0; //You've found me!
+            col.enabled = true;
+        };
+        if (Input.GetKeyUp(KeyCode.Mouse0) == true && thrown == false && active == true)
         {
             rb2.gravityScale = 1;
-
-            rb2.AddForce(scale * (Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position));
             thrown = true;
-        };
+            if(gameObject.name == "MultNord")
+            {
+                rb2.AddForce(scale * ((downspeed + Camera.main.ScreenToWorldPoint(Input.mousePosition)) - transform.position));
+            }
+            else if(gameObject.name == "MultSud")
+            {
+                rb2.AddForce(scale * ((upspeed + Camera.main.ScreenToWorldPoint(Input.mousePosition)) - transform.position));
+            }
+            else
+            {
+                rb2.AddForce(scale * (Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position));
+            }
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
