c31190dcea61291a57dab72bc6d0660e223b178f
Eric <ermorgan@protonmail.com>
2021-01-05 20:24:42 -0800

    Using placeholder, illegally acquired assets, we have a more visual shot selector

---
+++ Assets/Eric/MultSentry.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultSentry : MonoBehaviour
+{
+    public Sprite check;
+    public Sprite cross;
+    public GameObject basic;
+    // Start is called before the first frame update
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (basic.GetComponent<BallThrow>().active == true)
+        {
+            GetComponent<SpriteRenderer>().sprite = check;
+        }
+        else
+        {
+            GetComponent<SpriteRenderer>().sprite = cross;
+        }
+    }
+}
