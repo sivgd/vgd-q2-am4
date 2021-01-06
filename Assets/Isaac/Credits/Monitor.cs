402776eded6ceb3df99d7ba502cdd26cb1381d7a
isaac <isaac@DESKTOP-M97TLDN.local>
2021-01-06 09:54:14 -0800

    tempoary credit stuff from my last game. though I made credit roller work

---
+++ Assets/Isaac/Credits/Monitor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Monitor : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+
+            SceneManager.LoadScene("CreditRoller"); //Requires "Using" (see above)
+
+        }
+    }
+}
