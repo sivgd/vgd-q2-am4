734765c79f2304b06946a2b1b125eb807f600818
isaac <isaac@DESKTOP-M97TLDN.local>
2021-01-18 17:51:13 -0800

    menu stuff

---
+++ Assets/SetVolume.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+
+public class SetVolume : MonoBehaviour
+{
+    public AudioMixer mixer;
+
+    public SliderJoint2D volumeSlider; 
+    
+    
+    
+    
+    public void SetLevel (float sliderValue)
+    {
+
+        mixer.SetFloat("MusicVol", Mathf.Log10(sliderValue) * 20);
+    }
+
+   
+
+}   
+
+
