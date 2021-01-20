70e3525d19f0e993fff33590a927d2e339cdf223
Eric <ermorgan@protonmail.com>
2021-01-20 12:26:13 -0800

    Update EndlessUnlock.cs, EndlessUnlock.cs.meta, and 3 more files...

---
+++ Assets/EndlessUnlock.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndlessUnlock : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnEnable()
+    {
+        /*if(PlayerPrefs.GetInt("endless") == 0)
+        {
+            GetComponent<>().color = new Color(156, 0, 0, 0);
+        }
+        else
+        {
+            GetComponent<SpriteRenderer>().color = new Color(156, 0, 0, 255);
+        }*/
+    }
+}
