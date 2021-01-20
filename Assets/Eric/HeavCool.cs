89ffc573fa64f3e48ad4b422878f0f4b873f013b
Eric <ermorgan@protonmail.com>
2021-01-19 20:50:28 -0800

    Final Cooldown implementation

---
+++ Assets/Eric/HeavCool.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeavCool : MonoBehaviour
+{
+    private float cooldownmax;
+    private float cooldown;
+    public float percent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cooldownmax = GameObject.Find("bigSnow(k)").GetComponent<BallThrow>().baseCooldown;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cooldown = GameObject.Find("bigSnow(k)").GetComponent<BallThrow>().cooldown;
+        if (cooldown <= 0)
+        {
+            transform.localScale = new Vector3(5, 1, 1);
+            GetComponent<SpriteRenderer>().color = new Color(0, 255, 0);
+        }
+        else
+        {
+            percent = cooldown / cooldownmax;
+            transform.localScale = new Vector3((5 - (5 * percent)), 1, 1);
+            GetComponent<SpriteRenderer>().color = new Color((255 * percent), (255 - (255 * percent)), 0);
+        }
+    }
+}
